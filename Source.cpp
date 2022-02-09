2029a07352238e3658e694beb51a5887570b5cab
AthenaKas <99173750+AthenaKas@users.noreply.github.com>
2022-02-09 14:48:18 +0200

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+﻿#include <iostream>
 #include <iomanip>
 #include <string>
 
@@ -12,37 +12,42 @@ using std::fixed;
 
 struct data
 {
-	string vard, pav; 
-	int paz[10], egz, n; //n - pazymiu kiekis
-	double vidrezult, medrezult;
+	string vard{}, pav{};
+	int paz[10]{}, egz{}; //n{}; //n - pazymiu kiekis
+	double vidrezult{}, medrezult{};
 
 };
 
 //---
-void ivestis(data& a);
-void galutinisvid(data& a);
-void galutinismed(data& a);
+void ivestis(data& a, int& n);
+void galutinisvid(data& a, int& n);
+void galutinismed(data& a, int& n);
 void rezultatai(data& a);
 //---
 
 int main()
 {
 	data* mas;
-	mas = new data[2];
-
-	for (data* A = mas; A < mas + 2; A++)
+	mas = new data[1];
+	string anw;
+	int n = 1; //kiekis pazymiu
+	
+	for (data* A = mas; A < mas + 1; A++)
 	{
-		ivestis(*A);
+		ivestis(*A, n);
+		cout << "Ar norite ivesti dar vieno studento duomenis: "; cin >> anw;
+		if (anw != "yes") break;
+
 	}
 
-	for (data* A = mas; A < mas + 2; A++)
+	for (data* A = mas; A < mas + 1; A++)
 	{
-		galutinisvid(*A);
+		galutinisvid(*A, n);
 	}
 
-	for (data* A = mas; A < mas + 2; A++)
+	for (data* A = mas; A < mas + 1; A++)
 	{
-		galutinismed(*A);
+		galutinismed(*A, n);
 	}
 
 	cout << setw(20) << "Vardas";
@@ -50,7 +55,7 @@ int main()
 	cout << setw(20) << "Galutinis (Vid.)";
 	cout << setw(20) << "Galutinis (Med.)" << endl;
 	cout << endl;
-	for (data* A = mas; A < mas + 2; A++)
+	for (data* A = mas; A < mas + 1; A++)
 	{
 		rezultatai(*A);
 	}
@@ -58,47 +63,59 @@ int main()
 	delete[] mas;
 }
 
-void ivestis(data& a)
+void ivestis(data& a, int& n)
 {
+	string anw;
 	cout << "Iveskite varda: "; cin >> a.vard;
 	cout << "Iveskite pavarde: "; cin >> a.pav;
 
-	cout << "Iveskite kiek pazymiu turi studentas: ";  cin >> a.n;
-	for (int i = 0; i < a.n; i++)
+	//cout << "Iveskite kiek pazymiu turi studentas: ";  cin >> a.n;
+	cout << "Iveskite iki 10 pazymiu, kai nusprendziate, kad pazymiu uztenka rasykite 0 " << endl;
+
+	for (int i = 0; i < n; i++)
 	{
 		cout << "Iveskite " << i + 1 << " pazymi: ";
 		cin >> a.paz[i];
+		if (a.paz[i] == 0)
+		{
+			break;
+		}
+		else {
+			n++;
+		}
 	}
+	n--;
 
 	cout << "Iveskite egzamino ivertinima: "; cin >> a.egz;
+
 }
 
-void galutinisvid(data& a) //su vidurkiu
+void galutinisvid(data& a, int& n) //su vidurkiu
 {
 	double vid = 0;
 	int suma = 0;
 
-	for (int i = 0; i < a.n; i++)
+	for (int i = 0; i < n; i++)
 	{
 		suma += a.paz[i];
 	}
 
-	vid = suma / (double)a.n;
+	vid = suma / (double)n;
 
 	a.vidrezult = 0.4 * vid + 0.6 * a.egz;
 }
 
-void galutinismed(data& a)//su mediana
+void galutinismed(data& a, int& n)//su mediana
 {
 	double med;
 
-	if (a.n % 2 != 0)
+	if (n % 2 != 0)
 	{
-		med = a.paz[a.n / 2];
+		med = a.paz[n / 2];
 	}
 	else
 	{
-		med =(double)(a.paz[a.n / 2] + a.paz[(a.n / 2) - 1]) / 2;
+		med = (double)(a.paz[n / 2] + a.paz[(n / 2) - 1]) / 2;
 	}
 
 	a.medrezult = 0.4 * med + 0.6 * a.egz;
