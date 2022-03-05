64d4aed0cadbea56afa77932bc91f3d818f263d9
AthenaKas <99173750+AthenaKas@users.noreply.github.com>
2022-03-05 13:18:50 +0200

    Add files via upload

--- Source-vektoriai.cpp
+++ Source-vektoriai.cpp
@@ -1,46 +1,5 @@
-//Source-vektoriai MAIN
-#include <iostream>
-#include <iomanip>
-#include <string>
-#include <vector>
-#include <iterator>
-#include <sstream>
-#include <fstream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <algorithm>
-#include <ctime>
-#include <fstream>
-#include <random>
-#include <chrono>
-
-using std::cout;
-using std::cin;
-using std::string;
-using std::setw;
-using std::endl;
-using std::setprecision;
-using std::fixed;
-using std::sort;
-using std::vector;
-using std::ifstream;
-
-struct data
-{
-	string vard{}, pav{};
-	int paz[20]{}, egz{};
-	double vidrezult{}, medrezult{};
-};
-
-const char CDfv[] = "kursiokai.txt";
-
-//---
-void ivestis(data& a, int& n);
-void galutinisvid(data& a, int& n);
-void galutinismed(data& a, int& n);
-bool rikiavimas(const data& a, const data& b);
-void rezultatai(data& a);
-//---
+﻿//Source-vektoriai MAIN
+#include "header.h"
 
 
 int main()
@@ -58,6 +17,25 @@ int main()
 	
 	ifstream fd(CDfv);
 
+	if((anw2 == "y")||( anw2 == "Y"))
+	{
+		
+		try {
+			if (fd)
+			{
+
+			}
+			else
+			{
+				throw 505;
+			}
+		}
+		catch (...)//tikrina ar yra failas
+		{
+			cout << "Sukurkite studentu duomenu faila";
+			return 1;
+		}
+	}
 
 	do {
 		if ((anw2 == "n") || (anw2 == "N"))
@@ -169,122 +147,3 @@ int main()
 
 		sarasas.clear();
 }
-void ivestis(data& a, int& n)
-{
-	string anw; //ats: suvesti ar generuoti pazymius
-	char x; //skirtas patikrinti ar ivedamas pazymys yra skaicius
-	int y = 0; // generacija/suvedimas
-
-
-	cout << "Iveskite varda: "; cin >> a.vard;
-	cout << "Iveskite pavarde: "; cin >> a.pav;
-
-	cout << "Ar atsitiktinai sugeneruoti pazymius ? [y/n] "; cin >> anw;
-
-	do
-	{
-
-		if ((anw == "n") || (anw == "N"))
-		{
-			n = 1;
-			cout << "Iveskite nuo 1 iki 10 pazymiu, kai nusprendziate, kad pazymiu uztenka rasykite: [s] " << endl;
-
-			for (int i = 0; i < n; i++)
-			{
-				cout << "Iveskite " << i + 1 << " pazymi: ";
-				cin >> x;
-				if ((x == 's') || (x == 'S'))//jei iveda [s]
-				{
-					if (n > 1) //daugiau nebegalima vesti pazymiu
-					{
-						break;
-					}
-					else
-					{
-						cout << "Iveskite nuo 1 iki 10 pazymiu, kai nusprendziate, kad pazymiu uztenka rasykite : [s]" << endl; //ivesta maziau nei vienas pazymys
-						i--;
-					}
-				}
-				else
-				{
-					if (isdigit(x)) //tiktina ar ivesta reiksme skaicius
-					{
-						a.paz[i] = x - 48;
-						n++;
-					}
-					else
-					{
-						cout << "Iveskite nuo 1 iki 10 pazymiu, kai nusprendziate, kad pazymiu uztenka rasykite : [s]" << endl; //ivede ne skaiciu
-						i--;
-					}
-
-				}
-			}
-			n--;
-			cout << "Iveskite egzamino ivertinima: "; cin >> a.egz;
-
-			y++;
-		}
-		else if ((anw == "y") || (anw == "Y"))
-		{
-			using hrClock = std::chrono::high_resolution_clock;
-			std::mt19937 mt(static_cast<long unsigned int>(hrClock::now().time_since_epoch().count()));
-			std::uniform_int_distribution<int> dist(1, 10);
-
-			n = dist(mt);
-
-			for (int i = 0; i < n; i++) {
-				a.paz[i] == dist(mt);
-			}
-
-			a.egz = dist(mt);
-
-			y++;
-		}
-		else
-		{
-			cout << "Suvesti ar atsitiktinai generuoti pazymius? [y/n] "; cin >> anw; //jei nebuvo irasyta suvesti/generuoti
-		}
-	} while (y == 0);
-
-}
-void galutinisvid(data& a, int& n) //su vidurkiu
-{
-	double vid = 0;
-	int suma = 0;
-
-	for (int i = 0; i < n; i++)
-	{
-		suma += a.paz[i];
-	}
-
-	vid = suma / (double)(n);
-
-	a.vidrezult = 0.4 * vid + 0.6 * a.egz;
-}
-void galutinismed(data& a, int& n)//su mediana
-{
-	double med;
-	sort(a.paz, a.paz + n);
-
-	if (n % 2 != 0)
-	{
-		med = (double)a.paz[n / 2];
-	}
-	else
-	{
-		med = (double)(a.paz[(n - 1) / 2] + a.paz[n / 2]) / 2;
-	}
-
-	a.medrezult = 0.4 * med + 0.6 * a.egz;
-
-}
-void rezultatai(data& a)
-{
-	cout << setw(20) << a.vard << " | " << setw(20) << a.pav << " | ";
-
-	cout << setw(20) << setprecision(2) << fixed << a.vidrezult << " | ";
-
-	cout << setw(20) << setprecision(2) << fixed << a.medrezult << endl;
-}
-bool rikiavimas(const data& a, 
